397c3990e189000453b5e55639f014bcb21eace9
Oscar Jurado <oscarvmw@gmailcom>
2024-02-17 15:31:39 -0500

    Add routes needed

--- Src/app.py
+++ Src/app.py
@@ -3,5 +3,25 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
+    return "<h2>Tasks App</h2>"
+
+@app.route("/tasks/")
+def tasks_list():
+    return []
+
+@app.route("/tasks/<int:task_id>", methods = ['GET', 'PATCH'])
+def get_task():
+    return {}
+
+@app.route("/tasks/add", methods = ['POST'])
+def add_task():
+    return "<p>Hello, World!</p>"
+
+@app.route("/task/update", methods = ['PUT', 'PATCH'])
+def update_task():
+    return "<p>Hello, World!</p>"
+
+@app.route("/task/<int:task_id>", methods = ['DELETE'])
+def delete_task():
     return "<p>Hello, World!</p>"
